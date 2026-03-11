268222bc87103476deaa7d72abf268996593341f
ilinca-onofrei <onofrei_ilinca@yahoo.com>
2026-03-11 08:49:51 +0200

    Primul meu commit

--- main.cpp
+++ main.cpp
@@ -3,53 +3,116 @@
 #include "include/Example.h"
 // This also works if you do not want `include/`, but some editors might not like it
 // #include "Example.h"
+#include <iostream>
+#include <array>
+#include <vector>
+#include <string>
+
+class Adresa {
+private:
+    std::string oras;
+    std::string strada;
+    int numar;
+public:
+    Adresa(std::string oras_, std::string strada_, int numar_)
+        :oras{oras_}, strada{strada_}, numar{numar_} {}
+    friend std::ostream& operator<<(std::ostream& os, const Adresa& a) {
+        os <<a.strada<<" nr. "<<a.numar<<" , "<<a.oras;
+        return os;
+    }
+};
+
+class Haina {
+private:
+    std::string denumire;
+    std::string marime;
+    double pret;
+public:
+    Haina(std::string denumire_, std::string marime_, double pret_)
+        : denumire{denumire_}, marime{marime_}, pret{pret_} {
+        std::cout<<"S a creat haina: "<<denumire<<"\n";
+    }
+
+    Haina(const Haina& other)
+        : denumire{other.denumire}, marime{other.marime}, pret{other.pret} {
+        std::cout<<"CC Haina: S a copiat "<<denumire<<"\n";
+    }
+
+    Haina& operator=(const Haina& other) {
+        if (this != &other) {
+            denumire=other.denumire;
+            marime=other.marime;
+            pret=other.pret;
+        }
+        std::cout<<"op=Haina: S a atribuit "<<denumire<<"\n";
+        return *this; // AICI SE RETURNEAZA REFERINTA
+    }
+
+    ~Haina() {
+        std::cout<<"Destructorul Haina: "<<denumire<<" a fost distrus.\n";
+    }
+
+    void aplicaDiscount(double procent) {
+        if (procent > 0 && procent <= 100) {
+            pret -= pret * (procent / 100.0);
+        }
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Haina& h) {
+        os<< h.denumire<< " (" <<h.marime<< ") - "<<h.pret<<" lei";
+        return os;
+    }
+};
+
+class Manechin {
+private:
+    std::string numeManechin;
+    Haina hainaPurtata;
+public:
+    Manechin(std::string nume_, const Haina& haina_)
+        : numeManechin{nume_}, hainaPurtata{haina_} {}
+    friend std::ostream& operator<<(std::ostream& os, const Manechin& m) {
+        os <<"Manechinul "<<m.numeManechin<<" poarta: "<<m.hainaPurtata;
+        return os;
+    }
+    void schimbaTinuta(const Haina& hainaNoua) {
+        hainaPurtata=hainaNoua;
+        std::cout<<"Tinuta manechinului "<< numeManechin<< " a fost actualizata!\n";
+    }
+};
+
+class Boutique {
+private:
+    std::string numeMagazin;
+    Adresa locatie;
+    Manechin vitrina;
+public:
+    Boutique(std::string nume_, const Adresa& adr_, const Manechin& m_)
+        : numeMagazin{nume_}, locatie{adr_}, vitrina{m_} {}
+
+    friend std::ostream& operator<<(std::ostream & os, const Boutique& b) {
+        os <<"***"<<b.numeMagazin<<"***\n";
+        os <<"Locatie: "<<b.locatie<<"\n";
+        os <<"In vitrina: "<<b.vitrina<<"\n";
+        return os;
+    }
+
+};
 
 int main() {
-    std::cout << "Hello, 1311 la 8 dimineata!\n";
-    Example e1;
-    e1.g();
-    std::array<int, 100> v{};
-    int nr;
-    std::cout << "Introduceți nr: ";
-    /////////////////////////////////////////////////////////////////////////
-    /// Observație: dacă aveți nevoie să citiți date de intrare de la tastatură,
-    /// dați exemple de date de intrare folosind fișierul tastatura.txt
-    /// Trebuie să aveți în fișierul tastatura.txt suficiente date de intrare
-    /// (în formatul impus de voi) astfel încât execuția programului să se încheie.
-    /// De asemenea, trebuie să adăugați în acest fișier date de intrare
-    /// pentru cât mai multe ramuri de execuție.
-    /// Dorim să facem acest lucru pentru a automatiza testarea codului, fără să
-    /// mai pierdem timp de fiecare dată să introducem de la zero aceleași date de intrare.
-    ///
-    /// Pe GitHub Actions (bife), fișierul tastatura.txt este folosit
-    /// pentru a simula date introduse de la tastatură.
-    /// Bifele verifică dacă programul are erori de compilare, erori de memorie și memory leaks.
-    ///
-    /// Dacă nu puneți în tastatura.txt suficiente date de intrare, îmi rezerv dreptul să vă
-    /// testez codul cu ce date de intrare am chef și să nu pun notă dacă găsesc vreun bug.
-    /// Impun această cerință ca să învățați să faceți un demo și să arătați părțile din
-    /// program care merg (și să le evitați pe cele care nu merg).
-    ///
-    /////////////////////////////////////////////////////////////////////////
-    std::cin >> nr;
-    /////////////////////////////////////////////////////////////////////////
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "v[" << i << "] = ";
-        std::cin >> v[i];
-    }
-    std::cout << "\n\n";
-    std::cout << "Am citit de la tastatură " << nr << " elemente:\n";
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "- " << v[i] << "\n";
-    }
-    ///////////////////////////////////////////////////////////////////////////
-    /// Pentru date citite din fișier, NU folosiți tastatura.txt. Creați-vă voi
-    /// alt fișier propriu cu ce alt nume doriți.
-    /// Exemplu:
-    /// std::ifstream fis("date.txt");
-    /// for(int i = 0; i < nr2; ++i)
-    ///     fis >> v2[i];
-    ///
-    ///////////////////////////////////////////////////////////////////////////
+
+    Haina h1{"Rochie de seara", "M", 250.0};
+    Haina h2{"Sacou elegant", "S", 180.0};
+    Adresa adr{"Bucuresti","Calea Victoriei",101};
+    std::cout <<h1<< "\n";
+    std::cout <<h2 <<"\n";
+    Haina h3{h1}; // constructor de COPIERE
+    h3=h2; // operator de atribuire
+    Manechin man{"Anna", h1}; //compunere-manechin
+    Boutique shop{"ChicAtelier", adr, man}; //compunere boutique
+    std::cout<<shop<<"\n";
+    h1.aplicaDiscount(10);
+    man.schimbaTinuta(h2);
+    std::cout << "\n*** Final ***\n";
     return 0;
 }
